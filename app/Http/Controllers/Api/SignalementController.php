6db218eee65cda3f0d4ff3cf7e13d446c3f075ba
marouaessaddiki <ssadikimarwa@gmail.com>
2026-07-28 16:46:19 +0100

    modificatiion crud

--- app/Http/Controllers/Api/SignalementController.php
+++ app/Http/Controllers/Api/SignalementController.php
@@ -13,10 +13,14 @@ class SignalementController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+   public function index()
+{
+    $signalements = Signalement::with(['user', 'incident', 'departement'])
+        ->latest()
+        ->get();
+
+    return response()->json($signalements);
+}
 
     /**
      * Store a newly created resource in storage.
@@ -25,10 +29,10 @@ class SignalementController extends Controller
 {
     $data = $request->validated();
 
-    // Associer automatiquement l'utilisateur connecté
+    // Associer l'utilisateur connecté
     $data['user_id'] = auth()->id();
 
-    // Upload de la photo (optionnel)
+    // Upload de la photo
     if ($request->hasFile('photo')) {
         $data['photo'] = $request->file('photo')->store('signalements', 'public');
     }
@@ -37,30 +41,51 @@ class SignalementController extends Controller
 
     return response()->json([
         'message' => 'Signalement créé avec succès.',
-        'data' => $signalement
+        'data' => $signalement,
     ], 201);
 }
     /**
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
+{
+    $signalement = Signalement::with(['user', 'incident', 'departement'])
+        ->findOrFail($id);
+
+    return response()->json($signalement);
+}
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(UpdateSignalementRequest $request, string $id)
+{
+    $signalement = Signalement::findOrFail($id);
+
+    $data = $request->validated();
+
+    if ($request->hasFile('photo')) {
+        $data['photo'] = $request->file('photo')->store('signalements', 'public');
     }
 
+    $signalement->update($data);
+
+    return response()->json([
+        'message' => 'Signalement mis à jour avec succès.',
+        'data' => $signalement,
+    ]);
+}
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+   public function destroy(string $id)
+{
+    $signalement = Signalement::findOrFail($id);
+
+    $signalement->delete();
+
+    return response()->json([
+        'message' => 'Signalement supprimé avec succès.'
+    ]);
+}
 }

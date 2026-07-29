0c5f116a3611360e6c7a64c7ef7199e494f42ad5
TAHAMOUA <mouaddinetaha@gmail.com>
2026-07-29 16:09:32 +0100

    DepartementController

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,20 @@ use App\Http\Controllers\Api\SignalementController;
 use App\Http\Controllers\Api\IncidentController;
 use App\Http\Controllers\Api\DepartementController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('signalements', SignalementController::class);
+
     Route::apiResource('incidents', IncidentController::class);
+
+    Route::apiResource('departements', DepartementController::class);
+
+    Route::patch(
+        '/signalements/{signalement}/statut',
+        [SignalementController::class, 'updateStatus']
+    );
 
     Route::post(
         '/incidents/validate-grouping',
@@ -19,4 +27,3 @@ Route::middleware('auth:sanctum')->group(function () {
     );
 
 });
-
